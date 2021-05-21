e6d51bbc8507764281728ae7741da9fae5e4d3a7
roi <roirein@post.bgu.ac.il>
2021-05-21 23:05:33 +0300

    set up for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,3 @@
-console.log("Client Side JavaScript is Loaded")
-
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-    response.json().then((data)=>{
-        console.log(data)
-    })
-})
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -17,7 +8,7 @@ const messageTwo = document.querySelector('#message-2')
         messageOne.textContent='Loading...'
         messageTwo.textContent=""
         const location = search.value
-        fetch('http://localhost:3000/Weather?address=' + location).then((response)=>{
+        fetch('/Weather?address=' + location).then((response)=>{
             response.json().then((data)=>{
                 if (data.error){
                     console.log(data.error)
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 const app = express()
 const viewsPath = path.join(__dirname,'../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
+const port = process.env.PORT || 3000
 
 app.set('view engine','hbs')
 app.set('views', viewsPath)
@@ -84,6 +85,6 @@ app.get('*', (req,res)=>{
         message: 'Page Not Found'
     })
 })
-app.listen(3000,()=>{
-    console.log('Server Started on Port 3000')
+app.listen(port,()=>{
+    console.log('Server Started on Port ' + port)
 })
